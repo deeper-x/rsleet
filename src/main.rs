0b0b6b815b3a26a39197db9af2b3738e11adf238
justorius <albertodeprezzo@gmail.com>
2025-03-08 17:12:00 +0100

    #409

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod probl35;
 mod probl125;
+mod probl409;
 
 fn main() {
     let nums = vec![1,2,3,4,5];
     let target = 3;
     println!("Problem #35 - Result: {}", probl35::solution(nums, target));
 
-    let in_str = " ".to_string();
-    println!("Problem #125 - Result: {}", probl125::solution(in_str));
+    let in_str_125 = " ".to_string();
+    println!("Problem #125 - Result: {}", probl125::solution(in_str_125));
+
+    let in_str_409 = "abccccdd".to_string();
+    println!("Problem #409 - Result: {}", probl409::solution(in_str_409));
 
 }
