91a87ec762f16833814462ed43739b6b0bfdb0b9
justorius <albertodeprezzo@gmail.com>
2025-03-10 11:05:00 +0100

    #88 - wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod probl35;
 mod probl125;
+mod probl88;
+
 
 fn main() {
     let nums = vec![1,2,3,4,5];
@@ -8,5 +10,7 @@ fn main() {
 
     let in_str = " ".to_string();
     println!("Problem #125 - Result: {}", probl125::solution(in_str));
+
+    probl88::solution(&mut vec![1,2,3,0,0], 5, &mut vec![1,2], 3);
 
 }
---
+++ src/probl88/mod.rs
@@ -0,0 +1,3 @@
+pub fn solution(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    
+} 
