46e0686dd38f76111ff57a974357a28befdb17ab
justorius <albertodeprezzo@gmail.com>
2025-03-15 12:20:52 +0100

    #88 refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,24 @@ mod probl58;
 mod probl121;
 
 fn main() {
-    let nums = vec![1,2,3,4,5];
-    let target = 3;
-    println!("Problem #35 - Result: {}", probl35::solution(nums, target));
+    // let nums = vec![1,2,3,4,5];
+    // let target = 3;
+    // println!("Problem #35 - Result: {}", probl35::solution(nums, target));
 
-    let in_str_125 = " ".to_string();
-    println!("Problem #125 - Result: {}", probl125::solution(in_str_125));
+    // let in_str_125 = " ".to_string();
+    // println!("Problem #125 - Result: {}", probl125::solution(in_str_125));
 
-    let in_str_409 = "abccccdd".to_string();
-    println!("Problem #409 - Result: {}", probl409::solution(in_str_409));
+    // let in_str_409 = "abccccdd".to_string();
+    // println!("Problem #409 - Result: {}", probl409::solution(in_str_409));
 
-    let in_str_20 = "{()[]}".to_string();
-    println!("Problem #20 - Result: {}", probl20::solution(in_str_20));
+    // let in_str_20 = "{()[]}".to_string();
+    // println!("Problem #20 - Result: {}", probl20::solution(in_str_20));
 
-    let in_str_58 = "Hello World".to_string();
-    println!("Problem #58 - Result: {}", probl58::solution(in_str_58));
+    // let in_str_58 = "Hello World".to_string();
+    // println!("Problem #58 - Result: {}", probl58::solution(in_str_58));
 
-    let in_prices = vec![2,5,4,7,9,10];
-    println!("Problem #121 - Result: {}", probl121::solution(in_prices));
+    // let in_prices = vec![2,5,4,7,9,10];
+    // println!("Problem #121 - Result: {}", probl121::solution(in_prices));
 
     let mut in_nums1_88 = vec![1,2,3,0,0];
     let mut in_nums2_88 = vec![2,5,6];
--- src/probl88/mod.rs
+++ src/probl88/mod.rs
@@ -1,17 +1,17 @@
 pub fn solution(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-    let mut i = m as isize - 1; // Last valid index in nums1
-    let mut j = n as isize - 1; // Last index in nums2
-    let mut k = (m + n) as isize - 1; // Last position in nums1
+    let mut x1 = m as isize - 1; 
+    let mut x2 = n as isize - 1; 
+    let mut x_all = (m + n) as isize - 1; 
 
-    while j >= 0 {
-        if i >= 0 && nums1[i as usize] > nums2[j as usize] {
-            nums1[k as usize] = nums1[i as usize];
-            i -= 1;
+    while x2 >= 0 {
+        if x1 >= 0 && nums1[x1 as usize] > nums2[x2 as usize] {
+            nums1[x_all as usize] = nums1[x1 as usize];
+            x1 -= 1;
         } else {
-            nums1[k as usize] = nums2[j as usize];
-            j -= 1;
+            nums1[x_all as usize] = nums2[x2 as usize];
+            x2 -= 1;
         }
-        k -= 1;
+        x_all -= 1;
     }
 } 
 
